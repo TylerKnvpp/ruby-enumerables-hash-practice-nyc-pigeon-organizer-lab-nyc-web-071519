00423121c182baaf8eb04378c2994fa06ef44b78
tyler knapp <tgknapp11@gmail.com>
2019-07-04 20:50:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,7 +11,7 @@ def nyc_pigeon_organizer(data)
         if !new_hash[name][key]
           new_hash[name][key] = []
         end
-        new_hash[name][key].push(new_value.to_s) 
+        new_hash[name][key] << new_value.to_s
 
       end
     end
