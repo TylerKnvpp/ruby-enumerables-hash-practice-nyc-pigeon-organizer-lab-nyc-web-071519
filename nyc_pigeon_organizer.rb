16a094f47064dab8ef3b59048c8fe6d808b3d875
tyler knapp <tgknapp11@gmail.com>
2019-07-04 20:48:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+        
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+        new_hash[name][key].push(new_value.to_s) 
+
+      end
+    end
+  end
+  puts new_hash
 end
